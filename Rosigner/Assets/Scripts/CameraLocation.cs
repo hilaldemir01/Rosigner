b1dd591e9046b0e9d17f39f758de25ed924b3156
hilaldemir01 <hilal2000__1907@hotmail.com>
2022-03-25 00:35:40 +0300

    Camera Height is fixed

--- Rosigner/Assets/Scripts/CameraLocation.cs
+++ Rosigner/Assets/Scripts/CameraLocation.cs
@@ -16,9 +16,35 @@ public class CameraLocation : MonoBehaviour
         wall1_length = Wall1_location.gameObject.transform.localScale.x;
         wall2_length = Wall2_location.gameObject.transform.localScale.x;
 
+        if (wall1_length>=wall2_length)
+        {
+            if (wall1_length < 15)
+            {
+                Camera.gameObject.transform.localPosition = new Vector3(wall1_length / 2, wall1_length * 1.5f, wall2_length / 2);
+            }
+            else
+            {
+                Camera.gameObject.transform.localPosition = new Vector3(wall1_length / 2, wall1_length * 1.10f, wall2_length / 2);
+            }
+        }
+        else if (wall1_length < wall2_length)
+        {
+            if (wall2_length < 15)
+            {
+                Camera.gameObject.transform.localPosition = new Vector3(wall1_length / 2, wall2_length * 1.5f, wall2_length / 2);
+                if (wall2_length < 5)
+                {
+                    Camera.gameObject.transform.localPosition = new Vector3(wall1_length / 2, wall2_length * 2.0f, wall2_length / 2);
+                }
 
-        Camera.gameObject.transform.localPosition = new Vector3(wall1_length/2 ,20, wall2_length/2);
-       
+            }
+            else
+            {
+                Camera.gameObject.transform.localPosition = new Vector3(wall1_length / 2, wall2_length * 1.25f, wall2_length / 2);
+            }
+
+        }
+
 
     }
 }
