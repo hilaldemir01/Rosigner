1d9d9e92a5cdb0921a53454b3cd453e6b01c0922
hilaldemir01 <hilal2000__1907@hotmail.com>
2022-04-02 01:33:18 +0300

    Register checks are added

--- Rosigner/Assets/Scripts/Registration.cs
+++ Rosigner/Assets/Scripts/Registration.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -17,7 +18,7 @@ public class Registration : MonoBehaviour
     
 
   void Start() {
-        
+       
         notificationTxt=GameObject.Find("Canvas/notification").GetComponent<Text>();
         notificationTxt.gameObject.SetActive(false);
 
@@ -31,38 +32,100 @@ public class Registration : MonoBehaviour
 
     IEnumerator Register()
     {
-        WWWForm form = new WWWForm();
-        form.AddField("unity", "register");
-        form.AddField("firstname", nameInput.text);
-        form.AddField("lastname", surnameInput.text);
-        form.AddField("gender", GameObject.Find("GenderInput").GetComponent<Dropdown>().value);
-        form.AddField("email", emailInput.text);
-        form.AddField("password", passwordInput1.text);
-        form.AddField("password1", passwordInput2.text);
-        notificationTxt.gameObject.SetActive(true);
+        // This part of the code is used to check whether e-mail is valid or not. For e-mail address to be valid, it should include "@" sign and after that
+        // there should be a "." sign following by a domain.
 
-        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/Unity_DB/userRegister.php", form))
+        Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        Match match = regex.Match(emailInput.text);
+        int passwordCheck = VerifyPassword();
+        // If e-mail address is valid, then 
+        if (match.Success || passwordCheck == 1)
         {
-            yield return www.SendWebRequest();
+            WWWForm form = new WWWForm();
+            form.AddField("unity", "register");
+            form.AddField("firstname", nameInput.text);
+            form.AddField("lastname", surnameInput.text);
+            form.AddField("gender", GameObject.Find("GenderInput").GetComponent<Dropdown>().value);
+            form.AddField("email", emailInput.text);
+            form.AddField("password", passwordInput1.text);
+            form.AddField("password1", passwordInput2.text);
+            notificationTxt.gameObject.SetActive(true);
 
-            if (www.isNetworkError || www.isHttpError)
+            using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/Unity_DB/userRegister.php", form))
             {
-                notificationTxt.text= "" + www.error;
-            }
-            else
-            {
-                if(www.downloadHandler.text.Contains("Registration is successful")){
-                    notificationTxt.text= "" + www.downloadHandler.text;
-                    yield return new WaitForSeconds(1);
-                    UnityEngine.SceneManagement.SceneManager.LoadScene("Login");
-                }else{
-                    notificationTxt.text="" + www.downloadHandler.text;
+                yield return www.SendWebRequest();
+
+                // This part of the code checks whether there exists a network or connection error with the database.
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    notificationTxt.text = "" + www.error;
+                }
+                else
+                {
+                    if (www.downloadHandler.text.Contains("Registration is successful"))
+                    {
+                        notificationTxt.text = "" + www.downloadHandler.text;
+                        yield return new WaitForSeconds(1);
+                        UnityEngine.SceneManagement.SceneManager.LoadScene("Login");
+                    }
+                    else
+                    {
+                        notificationTxt.text = "" + www.downloadHandler.text;
+                    }
                 }
             }
         }
+        else if(!(match.Success) || passwordCheck == 0)
+        {
+            notificationTxt.text = "Invalid E-mail Type";
+        }
+        
+    }
+    
+    // This method is used to check whether the password includes numbers, upper or lower letters, special characters and at least 8 digit long.
+    public int VerifyPassword()
+    {
+        var hasNumber = new Regex(@"[0-9]+");
+        var hasUpperChar = new Regex(@"[A-Z]+");
+        var hasMiniMaxChars = new Regex(@".{8,50}");
+        var hasLowerChar = new Regex(@"[a-z]+");
+        var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+        if (!hasLowerChar.IsMatch(passwordInput1.text))
+        {
+            notificationTxt.text = "Password should contain at least one lower case letter";
+            return 0;
+        }
+        else if (!hasUpperChar.IsMatch(passwordInput1.text))
+        {
+            notificationTxt.text = "Password should contain at least one upper case letter"; 
+            return 0;
+
+        }
+        else if (!hasMiniMaxChars.IsMatch(passwordInput1.text))
+        {
+            notificationTxt.text = "Password should not be less than 8 characters";
+            return 0;
+
+        }
+        else if (!hasNumber.IsMatch(passwordInput1.text))
+        {
+            notificationTxt.text = "Password should contain at least one numeric value";
+            return 0;
+
+        }
+        else if (!hasSymbols.IsMatch(passwordInput1.text))
+        {
+            notificationTxt.text = "Password should contain At least one special case characters";
+            return 0;
+        }
+
+        return 1;
+
     }
 
-   public async void VerifyInputs()
+    // This method is used to check whether input fields are empty or not. It also checks for the password lengths to be more than 8 digits.
+    public async void VerifyInputs()
     {
         if(nameInput.text.Length >0 && surnameInput.text.Length > 0 && emailInput.text.Length > 0 && genderInput.value != 0 && passwordInput1.text.Length >= 8 &&  passwordInput2.text.Length >= 8)
         {
