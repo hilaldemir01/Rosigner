a3e1a7e70915807e2ed5c56f5834f7a6696824fa
hilaldemir01 <hilal2000__1907@hotmail.com>
2022-04-06 18:57:19 +0300

    Wall Click fixed

--- Rosigner/Assets/Scripts/WallDefiner.cs
+++ Rosigner/Assets/Scripts/WallDefiner.cs
@@ -27,7 +27,7 @@ public class WallDefiner : MonoBehaviour
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if(Physics.Raycast(ray, out hit, 100.0f))
+        if(Physics.Raycast(ray, out hit, 100.0f) && CanvasDistance.activeSelf == false)
         {
             if(hit.transform != null)
             {
