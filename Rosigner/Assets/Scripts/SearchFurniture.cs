925658aa89a26bd9173df51c67a1cd5a92c26373
gulbensuslu <gulben24suslu@gmail.com>
2022-04-10 00:02:58 +0300

    added furniture image

--- Rosigner/Assets/Scripts/SearchFurniture.cs
+++ Rosigner/Assets/Scripts/SearchFurniture.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class SearchFurniture : MonoBehaviour
 {
     public string theName;
+    public Text furnitureCategory;
+    //public List<Text> furnitureCategory;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+          furnitureCategory=GameObject.Find("Canvas/CategoryLayout/FurnitureArea/Living Room Furniture/Inventory/Category").GetComponent<Text>();
+          furnitureCategory.gameObject.SetActive(true);
     }
     public void SearchBar(string searchFurniture)
     {
        theName = searchFurniture;
-       Debug.Log(""+theName);
+        Debug.Log(""+theName);
+       
+        if(theName==furnitureCategory.text){
+             Debug.Log("fu"+furnitureCategory.text); // Bir çok category için check edilmeli ve categoryi sonuna kadar doğru yazınca buluyor bulunca oraya scroll olmalı // 
+            
+        } 
     }
     // Update is called once per frame
     void Update()
