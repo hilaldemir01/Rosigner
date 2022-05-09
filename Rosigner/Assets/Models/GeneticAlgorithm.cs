e5ada74b51f783a224d946eae189bde5fd3cfcf3
hilaldemir01 <hilal2000__1907@hotmail.com>
2022-05-09 23:57:00 +0300

    Genetic Sharp Library Added

---
+++ Rosigner/Assets/Models/GeneticAlgorithm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Models
+{
+    public class GeneticAlgorithm
+    {
+        List<Wall> wallList = new List<Wall>();
+        List<RoomStructure> roomStructuresList = new List<RoomStructure>();
+        RoomStructureLocation roomStructureLocation = new RoomStructureLocation();
+        int[,] floorPlan;
+        public void startMatrix()
+        {
+            int coordinate1 = (int)wallList[0].WallLength;
+            int coordinate2 = (int)wallList[1].WallLength;
+            floorPlan = new int[coordinate1,coordinate2];
+        }
+    }
+}
