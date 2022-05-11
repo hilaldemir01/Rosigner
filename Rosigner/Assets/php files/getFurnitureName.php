5c35f34e925903006eddb89cb8377ab61c19421a
irfanboraneroglu <borannn_1999@hotmail.com>
2022-05-12 00:58:40 +0300

    new php files added

--- Rosigner/Assets/php files/getFurnitureName.php
+++ Rosigner/Assets/php files/getFurnitureName.php
@@ -7,10 +7,33 @@ if ($conn -> connect_errno) {
 }
 if($_POST['unity'] == "getFurnitureType"){
 	$furnitureID=$_POST['furnitureID'];
-	$query = "SELECT * FROM furnituretype WHERE FurnitureTypeID  ='".$furnitureID."';";
-	$queryResult = $conn->query($query);
-	$result = $queryResult->fetch_object(); 
-	$returnvalue = "$result->FurnitureType";
+
+	$furnitureIDArray = explode(';', $furnitureID);
+	$returnvalue ="";
+	
+	for ($x = 0; $x < count($furnitureIDArray); $x++) {
+			$query = "SELECT * FROM furnituretype WHERE FurnitureTypeID  ='".$furnitureIDArray[$x]."';";
+
+			$rows = array();
+
+			$queryResult = $conn->query($query);
+			if(mysqli_num_rows($queryResult)>0)
+			{
+				
+		    while ($rows = mysqli_fetch_array($queryResult, MYSQLI_ASSOC)) {
+			    
+			    $string1 = $rows['FurnitureType'];
+				
+					$returnvalue .= "$string1;";
+		  	}
+			}
+
+			//$queryResult = $conn->query($query);
+			//$result = $queryResult->fetch_object(); 
+			//$string1 = "$result->FurnitureType";
+			//$returnvalue .= "$string1;";
+	}
+
 	echo $returnvalue;
 }
 ?>
--- Rosigner/Assets/php files/roomStructuresInformation.php
+++ Rosigner/Assets/php files/roomStructuresInformation.php
@@ -6,27 +6,38 @@ if ($conn -> connect_errno) {
   echo "Failed to connect to database: " . $conn -> connect_error;
 }
 if($_POST['unity'] == "roomStructuresInformation"){
+	
 	$wallID=$_POST['wallID'];
-	$query = "SELECT * FROM roomstructure WHERE WallID ='".$wallID."';";
 
+	$wallIDArray = explode(';', $wallID);
 	$returnvalue ="";
-	$rows = array();
+	for ($x = 0; $x < count($wallIDArray); $x++) {
+  
+		$query = "SELECT * FROM roomstructure WHERE WallID ='".$wallIDArray[$x]."';";
 
-	$queryResult = $conn->query($query);
-	if(mysqli_num_rows($queryResult)>0)
-	{
+		
+		
+
+		$rows = array();
+
+		$queryResult = $conn->query($query);
+		if(mysqli_num_rows($queryResult)>0)
+		{
+				
 	    while ($rows = mysqli_fetch_array($queryResult, MYSQLI_ASSOC)) {
-	    	
-	        $string1 = $rows['RoomStructureID'];
-			$string2 = $rows['StrructureLength'];
-			$string3 = $rows['StrructureWidth'];
-			$string4 = $rows['RedDotDistance'];
-			$string5 = $rows['GroundDistance'];
-			$string6 = $rows['FurnitureTypeID'];
-			$string7 = $rows['WallID'];
-			$returnvalue .= "$string1;$string2;$string3;$string4;$string5;$string6;$string7;";
-	    }
-	}
+		    
+		    $string1 = $rows['RoomStructureID'];
+				$string2 = $rows['StrructureLength'];
+				$string3 = $rows['StrructureWidth'];
+				$string4 = $rows['RedDotDistance'];
+				$string5 = $rows['GroundDistance'];
+				$string6 = $rows['FurnitureTypeID'];
+				$string7 = $rows['WallID'];
+				$returnvalue .= "$string1;$string2;$string3;$string4;$string5;$string6;$string7;";
+	  	}
+		}
+	
+	}	
 
 	echo $returnvalue;
 }
