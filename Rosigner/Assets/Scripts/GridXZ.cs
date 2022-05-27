87bc0a005deda605b6588bef2a1256a430f75934
irfanboraneroglu <borannn_1999@hotmail.com>
2022-05-27 05:53:20 +0300

    some comments deleted

--- Rosigner/Assets/Scripts/GridXZ.cs
+++ Rosigner/Assets/Scripts/GridXZ.cs
@@ -42,17 +42,7 @@ public class GridXZ<TGridObject>
         bool showDebug = true;
         if (showDebug)
         {
-            /*
-           TextMesh[,] debugTextArray = new TextMesh[height, width];
-            for (int x = 0; x < gridArray.GetLength(0); x++) {
-                for (int z = 0; z < gridArray.GetLength(1); z++) {
-                    debugTextArray[x, z] = UtilsClass.CreateWorldText(gridArray[x, z]?.ToString(), null, GetWorldPosition(x, z) + new Vector3(cellSize, 0, cellSize) * .4f, 5, Color.white, TextAnchor.MiddleCenter, TextAlignment.Center);
-                }
-            }
-
-               OnGridObjectChanged += (object sender, OnGridObjectChangedEventArgs eventArgs) => {
-                debugTextArray[eventArgs.x, eventArgs.z].text = gridArray[eventArgs.x, eventArgs.z]?.ToString();
-            };*/
+            
             //Lines drawn to divide the place into the grid
             for (int x = 0; x < gridArray.GetLength(1); x++)
             { // 5
@@ -87,42 +77,7 @@ public class GridXZ<TGridObject>
     {
         return new Vector3(x, 0, z) * cellSize + originPosition;
     }
-    /*
-        public void GetXZ(Vector3 worldPosition, out int x, out int z) {
-            x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
-            z = Mathf.FloorToInt((worldPosition - originPosition).z / cellSize);
-        }
-
-        public void SetGridObject(int x, int z, TGridObject value) {
-            if (x >= 0 && z >= 0 && x < width && z < height) {
-                gridArray[x, z] = value;
-                TriggerGridObjectChanged(x, z);
-            }
-        }
-
-        public void TriggerGridObjectChanged(int x, int z) {
-            OnGridObjectChanged?.Invoke(this, new OnGridObjectChangedEventArgs { x = x, z = z });
-        }
-
-        public void SetGridObject(Vector3 worldPosition, TGridObject value) {
-            GetXZ(worldPosition, out int x, out int z);
-            SetGridObject(x, z, value);
-        }
-
-        public TGridObject GetGridObject(int x, int z) {
-            if (x >= 0 && z >= 0 && x < width && z < height) {
-                return gridArray[x, z];
-            } else {
-                return default(TGridObject);
-            }
-        }
-
-        public TGridObject GetGridObject(Vector3 worldPosition) {
-            int x, z;
-            GetXZ(worldPosition, out x, out z);
-            return GetGridObject(x, z);
-        }
-    */
+    
     public Vector2 ValidateGridPosition(Vector2 gridPosition)
     {
         return new Vector2(
